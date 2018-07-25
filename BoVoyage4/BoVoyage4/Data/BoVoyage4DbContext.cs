af2869ec1a1d35ea643b32930d94eafc47d34ca0
Vincent Deisz <vincentdeisz@hotmail.fr>
2018-07-25 12:57:47 +0200

    Ajout dossier Data avec classe BoVoyage4DbContext (rempli) + ajout dossier Migrations avec classe Configuration (rempli également)

---
+++ BoVoyage4/BoVoyage4/Data/BoVoyage4DbContext.cs
@@ -0,0 +1,36 @@
+﻿using BoVoyage4.Areas.BackOffice.Models;
+using BoVoyage4.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyage4.Data
+{
+    public class BoVoyage4DbContext : DbContext
+    {
+        public BoVoyage4DbContext() : base("gjvf")
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Voyage> Voyages { get; set; }    
+
+        public DbSet<VoyageFile> VoyageFiles { get; set; }
+
+        public DbSet<AgenceVoyage> AgenceVoyages { get; set; }
+
+        public DbSet<Destination> Destinations { get; set; }
+
+        public DbSet<DossierReservation> DossierReservations { get; set; }
+
+        public DbSet<Participant> Participants { get; set; }
+
+        public DbSet<Commercial> Commerciaux { get; set; }
+
+        public DbSet<AssuranceAnnulation> AssuranceAnnulations { get; set; }
+
+    }
+}
---
+++ BoVoyage4/BoVoyage4/Migrations/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using BoVoyage4.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyage4.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<BoVoyage4DbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    }
+}
