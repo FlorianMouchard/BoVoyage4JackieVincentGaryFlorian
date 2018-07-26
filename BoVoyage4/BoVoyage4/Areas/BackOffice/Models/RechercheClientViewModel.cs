83fe27405664a9f2398a139c337a397f613189b0
Florian Mouchard <florian.mouchard@sfr.fr>
2018-07-27 00:48:27 +0200

    Ajout du ViewModel RechercheClientViewModel

---
+++ BoVoyage4/BoVoyage4/Areas/BackOffice/Models/RechercheClientViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BoVoyage4.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyage4.Areas.BackOffice.Models
+{
+    public class RechercheClientViewModel
+    {
+        [Display(Name = "Nom")]        
+        public string Nom { get; set; }
+
+        [Display(Name = "Prénom")]        
+        public string Prenom { get; set; }
+
+        [Display(Name = "Né avant le")]       
+        [DataType(DataType.Date)]
+        public DateTime? NeAvantLe { get; set; }
+
+        [Display(Name = "Né après le")]        
+        [DataType(DataType.Date)]
+        public DateTime? NeApresLe { get; set; }
+
+        IEnumerable<Client> Clients { get; set; }
+    }
+}
